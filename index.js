8aaa5c8d01f73b71eec4de22c7d2517b92ab523d
Thiago Alves <thiago_marketingdigital@hotmail.com>
2021-08-02 16:48:14 -0300

    ref: configure files to load in bot

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
 require('dotenv').config()
-
-const Discord = require('discord.js');
-const config = require("./config.json");
+const Discord = require("discord.js");
+const fs = require("fs");
 
 const client = new Discord.Client();
+const config = require("./config.json");
 
-client.on("ready", () => {
-  client.user.setActivity(`on ${client.guilds.cache.size} servers`);
-  console.log(`Ready to serve on ${client.guilds.cache.size} servers, for ${client.users.cache.size} users.`);
+client.config = config;
+client.startTime = Date.now();
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  console.log('LOG', `Carregando o total de ${files.length} evento(s).`)
+  
+  files.forEach(file => {
+    // se o arquivo não for um arquivo JS, ignore;
+    if (!file.endsWith(".js")) return;
+    
+    // carrega o arquivo de evento
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    console.log('LOG', `Carregando evento: ${eventName}`);
+
+    client.on(eventName, event.bind(null, client));
+    delete require.cache[require.resolve(`./events/${file}`)];
+  });
 });
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (message.content.indexOf(config.prefix) !== 0) return;
+client.commands = new Discord.Collection();
 
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.log(err);
+  
+  files.forEach(file => {
+    if(!file.endsWith("js")) return;
 
-  switch (command) {
-    case "ping" :
-      message.channel.send('Pong!');
-      break;
-    case "bah" :
-      message.channel.send('Tche');
-      break;    
-  }
-});
+    let props = require(`./commands/${file}`);
+    console.log('LOG', `Carregando comando: ${props.help.name}`)
 
-client.on("debug", (e) => console.info(e));
+    if (props.init) props.init(client)
+    client.commands.set(props.help.name, props);
+  })
+})
+
+
 client.login(process.env.TOKEN);
